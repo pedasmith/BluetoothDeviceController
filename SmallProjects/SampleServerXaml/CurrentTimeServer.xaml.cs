13fc75d364c94de170600ce181b642284eb49734
Peter Smith <shipwrecksoftware@live.com>
2024-04-07 10:48:44 -0500

    emoved pointless using statements ^r^g

--- SmallProjects/SampleServerXaml/CurrentTimeServer.xaml.cs
+++ SmallProjects/SampleServerXaml/CurrentTimeServer.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using System.Threading;
 using Utilities;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.WebUI;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
--- Utilities/DataReaderReadStringRobust.cs
+++ Utilities/DataReaderReadStringRobust.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Storage.Streams;
 
 namespace Utilities
