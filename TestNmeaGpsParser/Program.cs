858145f8c30efbd12029a0837144d7140324c842
Peter Smith <shipwrecksoftware@live.com>
2025-05-25 14:04:28 -0700

    Starting to add a GPS NMEA parser

---
+++ TestNmeaGpsParser/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Parsers.Nmea;
+
+namespace TestNmeaGpsParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Nmea Gps parser");
+
+            var example = Parsers.Nmea.Nmea_Gps_Parser.Example_01;
+            var parser = new Nmea_Gps_Parser();
+            var lines = example.Split(new char[] { '\n' }); 
+            foreach (var line in lines)
+            {
+                var result = parser.Parse(line);
+            }
+        }
+    }
+}
