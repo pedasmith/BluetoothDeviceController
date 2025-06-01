27413b82d3b9cdf59420585f59623b4c83d8557a
Peter Smith <shipwrecksoftware@live.com>
2025-05-31 19:49:43 -0700

    Skeleton version of the GPS control. It's only barely hooked up, but at least it's using the right usings and will show up in the GPS test app.

---
+++ WinUI3Controls/GpsControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using BluetoothDeviceController.SerialPort;
+using Microsoft.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WinUI3Controls
+{
+    public sealed partial class GpsControl : UserControl, ITerminal
+    {
+        public GpsControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public event TerminalSendDataEventHandler OnSendData;
+
+        public void ErrorFromDevice(string error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceivedData(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDeviceStatus(string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
